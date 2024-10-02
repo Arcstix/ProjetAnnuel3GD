b89e41f9346792d9dd660bece1d4fbb6ec5bd39b
Arcstix <morvan.louis2001@gmail.com>
2024-10-02 19:43:51 +0200

    Import Package + mécanique shoot + transportation + déplacement
    Import de DoTween + Recorder. Un script Shoot et Movement possèdent plusieurs paramètres que vous pouvez changer comme vous voulez. Vous pouvez aussi déplacer la target dans la hierarchie pour modifier le shoot.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,35 +7,64 @@ public class Projectile : MonoBehaviour
 {
     private Rigidbody _rb;
     private Shooter _shooterRef;
+    private bool _hasCollide = false;
+    private Vector3 _directionRef;
 
     private void GetRigidbody()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
-    public void Init(bool useGravity, Shooter shooterRef)
+    public void Init(bool useGravity, Shooter shooterRef, Vector3 direction)
     {
         _shooterRef = shooterRef;
+        _directionRef = direction;
         if(_rb == null)
         {
             GetRigidbody();
         }
 
         _rb.useGravity = useGravity;
+
+        if (useGravity)
+        {
+            Throw(direction);
+        }
+        else
+        {
+            Move(direction);
+        }
+    }
+
+
+    private void Throw(Vector3 direction)
+    {
+        _rb.AddForce(direction * _shooterRef.ShootPower, ForceMode.Impulse);
+    }
+
+    private void Move(Vector3 direction)
+    {
+        _rb.velocity = direction * _shooterRef.ShootPower;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Platform"))
         {
-            _rb.useGravity = false;
-            _rb.velocity = Vector3.zero;
+            ProjectileCollide();
         }
-        else
-        {
-            Debug.Log("Projectile Destroy");
-            _shooterRef.ResetAbility();
-            Destroy(gameObject);
-        }
+    }
+
+    private void ProjectileCollide()
+    {
+        _hasCollide = true;
+        _rb.useGravity = false;
+        _rb.velocity = Vector3.zero;
+    }
+
+    public void StopMovement()
+    {
+        _rb.useGravity = false;
+        _rb.velocity = Vector3.zero;
     }
 }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using System;
+using DG.Tweening;
 
 // envoie un projectile et est transporté jusqu'à la position du projectile
 public class Shooter : MonoBehaviour
 {
     [Header("Projectile section")]
-    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Projectile _projectilePrefab;
     [SerializeField] private Transform _launchPosition;
+    [SerializeField] private Transform _target;
     [SerializeField] private bool _useGravity;
 
     [Header("Camera section")]
@@ -29,10 +31,98 @@ public class Shooter : MonoBehaviour
     [SerializeField] private bool _onTransportationState = false;
     [SerializeField] private bool _isDead = false;
 
+    private CharacterController _controller;
+    private Projectile currentProjectile;
+
+    public int ShootPower { get => _shootPower; set => _shootPower = value; }
+    public int TransportationSpeed { get => _transportationSpeed; set => _transportationSpeed = value; }
+    public bool OnTransportationState { get => _onTransportationState; set => _onTransportationState = value; }
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Debug.DrawLine(_launchPosition.position, _target.position);
+
+        if(!_isDead && !_hasShoot && !_onTransportationState)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ShootProjectile();
+                return;
+            }
+        }
+
+        if (_isShooting)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _isShooting = false;
+                currentProjectile.StopMovement();
+                return;
+            }
+        }
+
+        if (_hasShoot && !_isShooting && !_onTransportationState)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ResetAbility();
+                TransportationState(true);
+                return;
+            }
+        }
+
+        if (_onTransportationState)
+        {
+            DoTransportation();
+        }
+    }
+
+    private void DoTransportation()
+    {        
+        Vector3 direction = (currentProjectile.transform.position - transform.position).normalized;
+        float distance = Vector3.Distance(transform.position, currentProjectile.transform.position);
+        _controller.Move(direction * Mathf.Min(distance + 5, TransportationSpeed) * Time.deltaTime);
+
+        if(distance < 0.5f)
+        {
+            TransportationState(false);
+            Destroy(currentProjectile.gameObject);
+        }
+    }
+
+    private void TransportationState(bool state)
+    {
+        _onTransportationState = state;
+    }
+
+    private void ShootProjectile()
+    {
+        ShootState();
+        currentProjectile = Instantiate(_projectilePrefab, _launchPosition.position, Quaternion.identity);
+        Vector3 direction = (_target.position - _launchPosition.position).normalized;
+        Debug.Log("Direction : " + direction);
+        currentProjectile.Init(_useGravity, this, direction);
+    }
+
+    private void ShootState()
+    {
+        _hasShoot = true;
+        _isShooting = true;
+    }
 
     public void ResetAbility()
     {
-        _isShooting = false;
         _hasShoot = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(_launchPosition.position, _target.position);
     }
 }
