416ad585da89a426202d7a4f3777ff5f1ffdb0d2
Arcstix <morvan.louis2001@gmail.com>
2025-01-12 23:14:13 +0100

    V1 Tool LD (pas finit) + Ajout d'un Processors Delta Time Scale pour l'input
    Le tool n'est pour l'instant pas utilisable il affiche tous les prefabs mais on ne peut rien y faire avec. Vous pouvez ajouter lors de la configuration des inputs dans la section Processors "Delta Time Scale" qui est censé permettre de rendre la lecture indépendant du framerate.

---
+++ Assets/Tool_LD/Editor/PaletteWindow.cs
@@ -0,0 +1,73 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Tool_LD.Editor
+{
+    public class PaletteWindow : EditorWindow
+    {
+        private SelectionWindow selectionWindow;
+        private int selectedIndex = 0; // Index of selected item
+        private readonly string[] options = new string[] { "Option 1", "Option 2", "Option 3" };
+        
+        [MenuItem("Tool/PaletteWindow")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<PaletteWindow>();
+            window.titleContent = new GUIContent("Palette Window");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(10);
+            CreateDropDown();
+            GUILayout.Space(10);
+
+            CreateButton();
+            GUILayout.Space(10);
+            
+            // Separator
+            GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(2));
+        }
+
+        private void CreateDropDown()
+        {
+            // Show the dropdown
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            selectedIndex = EditorGUILayout.Popup(selectedIndex, options);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        private void CreateButton()
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("New Palette"))
+            {
+                // Create new Palette Scriptable Object 
+                // Open the prefab selection window if not already open
+                CreateNewWindow();
+            }
+
+            if (GUILayout.Button("Save Palette"))
+            {
+                // Update the list in the selected palette SO
+            }
+
+            if (GUILayout.Button("Edit Palette"))
+            {
+                // Open the prefab selection window if not already open
+                CreateNewWindow();
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        private void CreateNewWindow()
+        {
+            selectionWindow = GetWindow<SelectionWindow>();
+            selectionWindow.titleContent = new GUIContent("Selection Window");
+            selectionWindow.Show();
+        }
+    }
+}
---
+++ Assets/Tool_LD/Script/Palette.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tool_LD.Script
+{
+    [CreateAssetMenu(fileName = "New Palette", menuName = "LD/New Palette", order = 0)]
+    public class Palette : ScriptableObject
+    {
+        public List<string> listOfPrefabPath = new List<string>();
+    }
+}
