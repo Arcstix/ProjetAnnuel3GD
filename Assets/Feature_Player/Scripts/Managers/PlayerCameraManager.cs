7c1e5efe239994be52b41a5ac4ccff4759521bd0
Arcstix <morvan.louis2001@gmail.com>
2025-01-21 12:52:02 +0100

    Correction dans la data de la Camera du PlayerSO
    J'ai retiré des variables inutiles qui datent de la 3ème personne et j'ai mis des infos pour que ça soit plus facile à comprendre

--- Assets/Feature_Player/Scripts/Managers/PlayerCameraManager.cs
+++ Assets/Feature_Player/Scripts/Managers/PlayerCameraManager.cs
@@ -40,32 +40,32 @@ public class PlayerCameraManager : MonoBehaviour, I_Initializer
         //Zoom();
     }
 
-    private void Zoom()
-    {
-        float zoomValue = inputProvider.GetAxisValue(2) * metricsManager.CurrentPlayerSO.CameraData.ZoomSensitivity;
-
-        currentTargetDistance = Mathf.Clamp(currentTargetDistance + zoomValue, cameraData.MinimumDistance, cameraData.MaximumDistance);
-
-        float currentDistance = framingTransposer.m_CameraDistance;
-
-        if (currentDistance == currentTargetDistance)
-        {
-            return;
-        }
-
-        float lerpedZoomValue = Mathf.Lerp(currentDistance, currentTargetDistance, cameraData.ZoomSmoothing * Time.deltaTime);
-
-        framingTransposer.m_CameraDistance = lerpedZoomValue;
-    }
+    // private void Zoom()
+    // {
+    //     float zoomValue = inputProvider.GetAxisValue(2) * metricsManager.CurrentPlayerSO.CameraData.ZoomSensitivity;
+    //
+    //     currentTargetDistance = Mathf.Clamp(currentTargetDistance + zoomValue, cameraData.MinimumDistance, cameraData.MaximumDistance);
+    //
+    //     float currentDistance = framingTransposer.m_CameraDistance;
+    //
+    //     if (currentDistance == currentTargetDistance)
+    //     {
+    //         return;
+    //     }
+    //
+    //     float lerpedZoomValue = Mathf.Lerp(currentDistance, currentTargetDistance, cameraData.ZoomSmoothing * Time.deltaTime);
+    //
+    //     framingTransposer.m_CameraDistance = lerpedZoomValue;
+    // }
 
     private void SetBaseFOV()
     {
         virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(virtualCamera.m_Lens.FieldOfView,
-            metricsManager.CurrentPlayerSO.CameraData.BaseFOV, 0.05f);
+            metricsManager.CurrentPlayerSO.CameraData.BaseFOV, metricsManager.CurrentPlayerSO.CameraData.SmoothingFactorBaseFOV);
     }
     
     private void SetTransportFOV(float time)
     {
-        virtualCamera.m_Lens.FieldOfView = metricsManager.CurrentPlayerSO.CameraData.TransitionTransportBaseFOV.Evaluate(time);
+        virtualCamera.m_Lens.FieldOfView = metricsManager.CurrentPlayerSO.CameraData.TransitionBaseTransportFOV.Evaluate(time);
     }
 }
