ad1416c5a5cf09edee222cad667f0c413fd7b7de
pochipo10 <pochipo10@hotmail.com>
2025-01-25 16:50:05 +0100

    modification Script "PlayerSoundManager"

--- Assets/Feature_Player/Scripts/Managers/PlayerSoundManager.cs
+++ Assets/Feature_Player/Scripts/Managers/PlayerSoundManager.cs
@@ -16,50 +16,58 @@ public class SoundManager : MonoBehaviour
         
     }
 
-
     public void tirDroit()
     {
         //Son qui se déclenche lorsequ'on tire la balle droite
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/TIr droit");
     }
 
     public void tirGauche()
     {
         //Son qui se déclenche lorsequ'on tire la balle gauche
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Tir gauche");
     }
     public void rappelDroit()
     {
         //Son qui se déclenche lorsequ'on rappelle la balle droite
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/rappel droit");
 
     }
     public void rappelGauche()
     {
         //Son qui se déclenche lorsequ'on rappelle la balle gauche
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/rappel gauche");
     }
 
     public void activationDroit()
     {
         //Son qui se déclenche lorsequ'on active la balle droite
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/activation droit");
     }
     public void activationGauche()
     {
         //Son qui se déclenche lorsequ'on active la balle gauche
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/activation gauche");
     }
     public void gravityFreeze()
     {
         //  Son lors qu"on rentre en etat gravity Freeze
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/gravity freeze");
     }
     public void dash()
     {
         // Son orse qu'on active un outil pour dash vers lui
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/dash");
     }
     public void chute()
     {
         //  Son qui s'activee à la fin du gravity freeze
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Player/Chute");
     }
 
     public void collision()
     {
-        //  !!! JE DEVRAI SUREMENT LE FAIRE MOI MEME CELUI CI !!!  Son qui s'activee qaund le joueur entre en collision avec quelque chose 
+        //  !!! JE DEVRAI SUREMENT LE FAIRE MOI MEME CELUI CI !!!  Son qui s'activee qaund le joueur entre en collision avec quelque chose
     }
 
  //INGREDIENTS 
@@ -67,15 +75,18 @@ public class SoundManager : MonoBehaviour
     public void blocCassablePortal()
     {
         //  Son lorsequ'on casse l'igrédient portail
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ingredients/Bloc cassable (portal)");
     }
 
     public void blocCassableViolet()
     {
         //  Son qui s'activee orsequ'on casse un bloc violet
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ingredients/Bloc cassable (violet)");
     }
     public void interrupteur()
     {
         //  Son qui s'activee lorseque l'interrupteur s'active
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ingredients/Interrupteur");
     }
 
     public void Collision()
@@ -85,6 +96,7 @@ public class SoundManager : MonoBehaviour
     public void porte()
     {
         //  Son qui s'activee lorse que la porte s'ouvre
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ingredients/Porte");
     }
 
 }
