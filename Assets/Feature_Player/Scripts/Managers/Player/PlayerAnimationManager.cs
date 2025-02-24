0bdc930f99e93ef6d76845f9964211573d1b1d16
Arcstix <morvan.louis2001@gmail.com>
2025-02-24 01:41:15 +0100

    Animation + mesh Arms + Scene Situation brouillon
    Il manque le script qui joue les animations des bras + y aura les metrics de la scène que j'ai faite dans le prochain push

---
+++ Assets/Feature_Player/Scripts/Managers/Player/PlayerAnimationManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerAnimationManager : MonoBehaviour
+{
+    private static readonly int AimL = Animator.StringToHash("Aim_L");
+    private static readonly int AimR = Animator.StringToHash("Aim_R");
+    private static readonly int Shoot = Animator.StringToHash("Shoot");
+    private static readonly int RecallL = Animator.StringToHash("Recall_L");
+    private static readonly int RecallR = Animator.StringToHash("Recall_R");
+    private static readonly int ActivateR = Animator.StringToHash("ActivateR");
+    private static readonly int ActivateL = Animator.StringToHash("Activate_L");
+    [SerializeField] private Animator animator;
+    
+    private PlayerAbilityManager abilityManager;
+
+    private void TriggerAimL()
+    {
+        animator.SetTrigger(AimL);
+    }
+
+    private void TriggerAimR()
+    {
+        animator.SetTrigger(AimR);
+    }
+
+    private void TriggerShoot()
+    {
+        animator.SetTrigger(Shoot);
+    }
+
+    private void TriggerRecallL()
+    {
+        animator.SetTrigger(RecallL);
+    }
+
+    private void TriggerRecallR()
+    {
+        animator.SetTrigger(RecallR);
+    }
+
+    private void TriggerActivateR()
+    {
+        animator.SetTrigger(ActivateR);
+    }
+
+    private void TriggerActivateL()
+    {
+        animator.SetTrigger(ActivateL);
+    }
+}
