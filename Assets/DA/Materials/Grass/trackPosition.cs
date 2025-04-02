2b1b8565dfe4ba1ebdebb97c35b1087d5819f49e
ISSA <121513541+xxFujixx@users.noreply.github.com>
2025-04-02 17:00:09 +0200

    Ajout Herbe
    Assets d'herbe reste a corriger le script pour la faire réagir

---
+++ Assets/DA/Materials/Grass/trackPosition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackPosition : MonoBehaviour
+{
+    private GameObject tracker;
+    private Material grassMat;
+    void Start()
+    {
+        grassMat = GetComponent<Renderer>().material;
+        tracker = GameObject.Find("tracker");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector3 trackerPos = tracker.GetComponent<Transform>().position;
+       grassMat.SetVector("_trackerPosition", trackerPos);
+    }
+}
