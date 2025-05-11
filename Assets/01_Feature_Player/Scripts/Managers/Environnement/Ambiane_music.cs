5411d66cd01b57cc62fe25679e82a1705caa65e3
pochipo10 <pochipo10@hotmail.com>
2025-05-12 00:50:06 +0200

    petite modif script ambiane_music

--- Assets/01_Feature_Player/Scripts/Managers/Environnement/Ambiane_music.cs
+++ Assets/01_Feature_Player/Scripts/Managers/Environnement/Ambiane_music.cs
@@ -1,82 +1,100 @@
-//using System;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Ambiane_music : MonoBehaviour
-//{
-//    void Awake()
-//    {
-//        // Cette fonction est appelée dès que le script est initialisé, même avant Start
-//        Debug.Log("Awake: Le jeu vient de se lancer.");
-//        MaFonction();
-//    }
+public class Ambiane_music : MonoBehaviour
+{
+    void Awake()
+    {
+        // Cette fonction est appelée dès que le script est initialisé, même avant Start
+        Debug.Log("Awake: Le jeu vient de se lancer.");
+        //MaFonction();
+    }
 
-//    void Start()
-//    {
-//        // Cette fonction est appelée au début du premier frame actif
-//        Debug.Log("Start: Début du jeu.");
+    void Start()
+    {
+        // Cette fonction est appelée au début du premier frame actif
+        Debug.Log("Start: Début du jeu.");
 
-//        //Musique qui se joue par défaut
-//        FMODUnity.RuntimeManager.PlayOneShot("event:/Musique/Musique ambiance");
+        //Son qui se joue toute la zone dans le désert
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Ambiance/Vent"); // CA FAUT LAISSER !
+    }
 
-//        //Son qui se joue toute la zone dans le désert
-//        FMODUnity.RuntimeManager.PlayOneShot("event:/Ambiance/Vent");
-//    }
+    public void MusiqueDeBase()
+    {
+        public string selectsoundMusiqueDeBase;
+        FMOD.Studio.EventInstance soundeventMusiqueDeBase;
 
-//    public void MusiquePointNarratif()
-//    {
-//        //Son qui se dEclenche lorsqu'on touche un buisson/herbe
-//        FMODUnity.RuntimeManager.PlayOneShot("event:/Ambiance/contact herbe");
-//    }
+        void start()
+        {
+            soundeventMusiqueDeBase = FMODUnity.RuntimeManager.CreateInstance(selectsoundMusiqueDeBase);
+        }
 
-//    public void MusiqueEnnemiProche()
-//    {
-//        //Musique lorsequ'on approche d'un ennemi
-//        FMODUnity.RuntimeManager.PlayOneShot("event:/Ambiance/contact herbe");
-//    }
+        void Update()
+        {
+            PlaysoundMusiqueDeBase();
+        }
 
-//    void MaFonction()
-//    {
-//        Debug.Log("MaFonction a été lancée !");
-//        // Ajoute ici le code que tu veux exécuter au lancement
-//    }
+        void PlaysoundMusiqueDeBase()
+        {
+            FMOD.Studio.PLAYBACK_STATE fmodPbState;
+            soundeventMusiqueDeBase.getPlaybackState(out fmodPbState);
+            if (fmodPbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            {
+                soundeventMusiqueDeBase.start();
+            }
 
-//    void AccordDash()
-//    {
-//        private void Start()
-//        {
-//            SonAccordDash = FMODUnity.RuntimeManager.CreateInstance("event:/Musique/Accords dash");
-//        }
+            if (Input.GetKeyUp(presstoplaysound)) // lorse que le joueur entre dans un triggerBox (avec le tag 'PhaseDynamique')
+            {
+                soundeventMusiqueDeBase.setParameterByName("Phase dynamique", 1f);
+            }
 
-//        SonAccordDash.setParameterByName("NB dash", 0f); // lancer ca quauand on a fait 2 transportations sans toucher le sol
-//        SonFleurSaisie.start(); 
+            if (Input.GetKeyUp(presstoplaysound)) // lorse que le joueur SORS de la triggerBox 
+            {
+                soundeventMusiqueDeBase.setParameterByName("Phase dynamique", 0f);
+            }
+        }
+    }
 
-//        SonAccordDash.setParameterByName("NB dash", 2f); // lancer ca quauand on a fait 5 transportations sans toucher le sol
-//        SonFleurSaisie.start(); 
 
-//        SonAccordDash.setParameterByName("NB dash", 4f); // lancer ca quauand on a fait 8 transportations sans toucher le sol
-//        SonFleurSaisie.start(); 
-//    }
 
-//    void AccordWallRun()
-//    {
-//        private void Start()
-//        {
-//            SonAccordWallrun = FMODUnity.RuntimeManager.CreateInstance("event:/Musique/Accords wall run");
-//        }
+    public void AccordDash()
+    {
+        void Start()
+        {
+            SonAccordDash = FMODUnity.RuntimeManager.CreateInstance("event:/Musique/Accords dash");
+        }
 
-//        SonAccordWallrun.setParameterByName("NB wallrun", 0f); // lancer ca quauand on a fait 2 wallruns sans toucher le sol
-//        SonAccordWallrun.start();
+        SonAccordDash.setParameterByName("NB dash", 0f); // lancer ca quauand on a fait 2 transportations sans toucher le sol
+        SonAccordDash.start();
 
-//        SonAccordWallrun.setParameterByName("NB wallrun", 0.5f); // lancer ca quauand on a fait 3  Wallruns sans toucher le sol
-//        SonAccordWallrun.start();
+        SonAccordDash.setParameterByName("NB dash", 2f); // lancer ca quauand on a fait 5 transportations sans toucher le sol
+        SonAccordDash.start();
 
-//        SonAccordWallrun.setParameterByName("NB wallrun", 1f); // lancer ca quauand on a fait 4 wallruns sans toucher le sol
-//        SonAccordWallrun.start();
-//    }
+        SonAccordDash.setParameterByName("NB dash", 4f); // lancer ca quauand on a fait 8 transportations sans toucher le sol
+        SonAccordDash.start();
+    }
 
-//    void AccordEnvole()
-//    {
-//        FMODUnity.RuntimeManager.PlayOneShot("event:/Musique/Accords envole");
-//    }
+    public void AccordWallRun()
+    {
+        void Start()
+        {
+            SonAccordWallrun = FMODUnity.RuntimeManager.CreateInstance("event:/Musique/Accords wall run");
+        }
 
-//}
+        SonAccordWallrun.setParameterByName("NB wallrun", 0f); // lancer ca quauand on a fait 2 wallruns sans toucher le sol
+        SonAccordWallrun.start();
+
+        SonAccordWallrun.setParameterByName("NB wallrun", 0.5f); // lancer ca quauand on a fait 3  Wallruns sans toucher le sol
+        SonAccordWallrun.start();
+
+        SonAccordWallrun.setParameterByName("NB wallrun", 1f); // lancer ca quauand on a fait 4 wallruns sans toucher le sol
+        SonAccordWallrun.start();
+    }
+
+    public void AccordEnvole()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Musique/Accords envole"); // lorse que le joueur fait une gagne beaucoup de hauteur
+    }
+
+}
